372c8adc6715d9f60e7b3e60cd01d3ad59e0f54e
Josh Varty <joshvarty@gmail.com>
2019-03-03 15:30:59 -0500

    Unfreeze before trainig

--- train.py
+++ train.py
@@ -18,6 +18,7 @@ def train(model):
             ds_tfms=tfms, size=96, num_workers=4).normalize(cancer_stats)
     
     learner = create_cnn(data, model, metrics=accuracy)
+    learner.unfreeze()
     learner.fit_one_cycle(30, max_lr=1e-2)
     learner.save(model.__name__ + "_saved")
     learner.export(model.__name__)
