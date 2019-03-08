399e1239555fe2d60247a0000936a81c8ee2c11d
Josh Varty <joshvarty@gmail.com>
2019-03-08 19:38:22 +0000

    Try with new cancer_stats

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ def train(model):
     #Remove symmetric warp
     tfms[0].remove(tfms[0][2])
 
-    cancer_stats = [[177.51475147222848, 138.6587881454333, 178.55802581840624], [54.9887577728556, 60.98731138460821, 71.97104127673265]]
+    cancer_stats = ([0.6961362802832489, 0.5437599535115032, 0.7002275522290441], [0.21564218734453175, 0.23916592699846356,0.2822393775558143])
     data = ImageDataBunch.from_csv(path, folder='train', csv_labels='train_labels.csv', suffix='.tif', valid_pct=0.1,
             ds_tfms=tfms, size=96, num_workers=4).normalize(cancer_stats)
     
@@ -25,7 +25,7 @@ def train(model):
 
 if __name__ == "__main__":
 
-    pre_trained_models = [models.resnet34, models.resnet50, models.resnet101, models.resnet152, models.densenet121, models.squeezenet1_1]
+    pre_trained_models = [models.resnet50, models.resnet101, models.resnet152, models.densenet121]
     for model in pre_trained_models:
         train(model)
     
